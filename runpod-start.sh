1468d1935c075c0b17fa9d67c7b3810fe56370b6
DigiRoninCollective <129343687+DigiRoninCollective@users.noreply.github.com>
2025-06-30 21:51:15 -0500

    Add monitor and vanity scripts; fix runpod start paths

--- runpod-start.sh
+++ runpod-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nvidia-smi -pm 1
-nvidia-smi -ac 5001,1410
-python /app/controller/monitor.py &
-/app/src/cuda/vanity --gpus 2
+nvidia-smi -pm 1 || true
+nvidia-smi -ac 5001,1410 || true
+python app/controller/monitor.py &
+./app/src/cuda/vanity --gpus 2
