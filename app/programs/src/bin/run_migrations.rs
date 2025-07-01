8f45e3d61ab91b68a054b343bc8f065dee0699b8
DigiRoninCollective <129343687+DigiRoninCollective@users.noreply.github.com>
2025-06-30 23:26:23 -0500

    Add Diesel connection utility

--- app/programs/src/bin/run_migrations.rs
+++ app/programs/src/bin/run_migrations.rs
@@ -1,5 +1,6 @@
 // programs/revenue-split/src/bin/run_migrations.rs
 use diesel_migrations::{embed_migrations, MigrationHarness};
+use vanity_addy::establish_connection;
 use std::error::Error;
 
 pub fn run() -> Result<(), Box<dyn Error>> {
